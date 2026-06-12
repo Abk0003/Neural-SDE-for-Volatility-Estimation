74bb2147ab948a2c5438918157dc39dd8b0fd919
Abk0003 <kashyap.abk03@gmail.com>
2026-06-12 12:41:10 +0530

    commit

--- neural_sde.py
+++ neural_sde.py
@@ -8,10 +8,16 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from scipy.stats import skew, kurtosis
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("cuda" if torch.cuda.is_available() else "cpu")
 data = yf.download("^NSEI", start="2000-01-01", end= "2025-12-31")
 dataVIX = yf.download("^INDIAVIX", start="2000-01-01", end= "2025-12-31")
-price = data["Close"].dropna()
-priceVIX = dataVIX["Close"].dropna()
+df = pd.DataFrame()
+df["nifty"] = data["Close"]
+df["vix"] = dataVIX["Close"]
+df = df.dropna()
+price = df["nifty"].dropna()
+priceVIX = df["vix"].dropna()
 returnsVIX = np.log((priceVIX/priceVIX.shift(1)).dropna())
 returns = np.log(price/price.shift(1))
 returns = returns.dropna()
@@ -23,29 +29,29 @@ mom_20 = lr.rolling(20).mean()
 lr = returns.values.flatten()
 lrVIX = returnsVIX.values.flatten()
 
-idx = int(0.8 * len(lr))
-r_train = lr[:idx]
-r_test = lr[idx:]
 dt = 1/252
-N = len(r_train)
 features = []
 for i in range(len(lr)):
-    rel_5 = rel_vol5[i]
-    rel_20 = rel_vol20[i]
-    m5 = mom_5[i]
-    m20 = mom_20[i]
+    rel_5 = rel_vol5.iloc[i]
+    rel_20 = rel_vol20.iloc[i]
+    m5 = mom_5.iloc[i]
+    m20 = mom_20.iloc[i]
     r = lr[i]
     vix = lrVIX[i]
-    if(i == 0):
+    if i == 0:
         vix_t = 0
     else:
         vix_t = (lrVIX[i] - lrVIX[i - 1]) / (lrVIX[i - 1] + 1e-8)
     skewi = skew(lr[max(0, i - 20):i]) if i > 1 else 0
     kurti = kurtosis(lr[max(0, i - 20):i]) if i > 1 else 0.0
-    f = [rel_vol5.iloc[i], rel_vol20.iloc[i], mom_5.iloc[i], mom_20.iloc[i], skewi, kurti, vix_t, vix,r]
+    f = [rel_5, rel_20, m5, m20, skewi, kurti,vix, vix_t, r]
     features.append(f)
 features = features[20:]
-features = torch.tensor(features)
+features = np.array(features)
+feat_mean = features.mean(axis=0)
+feat_std  = features.std(axis=0) + 1e-8
+features  = (features - feat_mean) / feat_std
+features = torch.tensor(features, dtype=torch.float32)
 
 class MLP(nn.Module):
     def __init__(self, in_dim, out_dim):
@@ -54,8 +60,8 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, out_dim)
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
         x = self.fc3(x)
         return x
 
@@ -73,7 +79,8 @@ class NeuralSDE(nn.Module):
         self.decoder = nn.Sequential(
             nn.Linear(latent_dim, 64),
             nn.Tanh(),
-            nn.Linear(64,1)
+            nn.Linear(64,1),
+            nn.Sigmoid()
         )
     def forward(self,x,h = None):
         if h is None:
@@ -81,25 +88,41 @@ class NeuralSDE(nn.Module):
         inp = torch.cat([h, x], dim=-1)
         mu = self.drift(inp)
         sigma = F.softplus(self.diff(inp))
-        dw = torch.randn_like(h) * np.sqrt(dt)
+        dw = torch.randn_like(h) * np.sqrt(dt) * 0.1
         h_new = h + mu * dt + sigma * dw
         out = self.decoder(h_new)
         return out, h_new
 
-model = NeuralSDE(9,64)
-optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+idx = int(0.8 * len(features))
+r_train = features[:idx]
+r_test = features[idx:]
+model = NeuralSDE(9,8).to(device)
+optimizer = torch.optim.Adam(model.parameters(),lr=3e-4)
 criterion = torch.nn.MSELoss()
-X = features[:idx]
-y = torch.tensor(lr[20:idx+20], dtype=torch.float32).unsqueeze(1)
-h = None
+X = features[:idx].to(device)
+y = np.array(lr[21:idx+21]**2)
+y_mean = np.mean(y)
+y_std = np.std(y)
+y = (y - y_mean)/y_std
+y = torch.tensor(y, dtype=torch.float32).unsqueeze(1).to(device)
+
 for epoch in range(200):
-    optimizer.zero_grad()
-    out, h = model(X,h)
-    h.detach()
-    loss = criterion(out, y)
-    loss.backward()
-    optimizer.step()
-    print(loss.item())
+    epoch_loss = 0.0
+    h = None
+    lossT = 0
+    for t in range(len(X)):
+        out, h = model(X[t:t+1],h)
+        lossT += criterion(out, y[t:t+1])
+        if (t+1)%128 == 0:
+            epoch_loss += lossT.item()
+            optimizer.zero_grad()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            lossT.backward()
+            optimizer.step()
+            h = h.detach()
+            lossT = 0
+    print(f"Epoch : {epoch}, Loss : {epoch_loss}")
+
 
 
 
