13524cd61cbf025e66536592c19dab041df65c8e
Abk0003 <kashyap.abk03@gmail.com>
2026-06-12 05:52:47 +0530

    commit

--- neural_sde.py
+++ neural_sde.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+from scipy.stats import skew, kurtosis
 
 data = yf.download("^NSEI", start="2000-01-01", end= "2025-12-31")
 dataVIX = yf.download("^INDIAVIX", start="2000-01-01", end= "2025-12-31")
@@ -22,38 +23,60 @@ r_train = lr[:idx]
 r_test = lr[idx:]
 dt = 1/252
 N = len(r_train)
-
+rel_vol5 = lr.rolling(5).std()
+rel_vol20 = lr.rolling(20).std()
+mom_5 = lr.rolling(5).mean()
+mom_20 = lr.rolling(20).mean()
 features = []
 for i in range(len(lr)):
-    rel_vol5 = lr.rolling(5).std()
-    rel_vol20 = lr.rolling(20).std()
-    mom_5 = lr.rolling(5).mean()
-    mom_20 = lr.rolling(20).mean()
+    rel_5 = rel_vol5[i]
+    rel_20 = rel_vol20[i]
+    m5 = mom_5[i]
+    m20 = mom_20[i]
     r = lr[i]
     vix = lrVIX[i]
-    vix_t = (lrVIX - lrVIX.shift(1))/lrVIX.shift(1)[i]
-    skew = np.skew(lr)[i]
-    kurt = np.kurtosis(lr)[i]
-    f = [rel_vol5, rel_vol20, mom_5, mom_20, skew, kurt, vix_t, vix,r]
+    if(i == 0):
+        vix_t = 0
+    else:
+        vix_t = (lrVIX[i] - lrVIX[i - 1]) / (lrVIX[i - 1] + 1e-8)
+    skewi = skew(lr[max(0, i - 20):i])
+    kurti = kurtosis(lr[max(0, i - 20):i])
+    f = [rel_vol5, rel_vol20, mom_5, mom_20, skewi, kurti, vix_t, vix,r]
     features.append(f)
 
 features = torch.tensor(features)
+
+class MLP(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(in_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, out_dim)
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 class NeuralSDE(nn.Module):
-    def __init__(self,in_channels,out_channels):
+    def __init__(self,feature_dim,latent_dim):
         super(NeuralSDE, self).__init__()
-        self.net = nn.Sequential(
-            nn.Linear(in_channels,64),
-            nn.ReLU(),
-            nn.Linear(64,64),
-            nn.ReLU(),
-            nn.Linear(64,64),
-            nn.ReLU(),
-            nn.Linear(64,out_channels),
+        self.latent_dim = latent_dim
+        self.encoder = nn.Sequential(
+            nn.Linear(feature_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, latent_dim)
         )
+
     def forward(self,x):
         out = self.net(x)
-        out = nn.Softmax(out)
+        out = F.softmax(out)
         return out
+
+model = NeuralSDE(8,1)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+criterion = torch.nn.MSELoss()
+
 
 
 
